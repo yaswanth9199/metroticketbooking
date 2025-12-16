b606eddc229108fe1fb8c2cff20739027c3c3aab
D.Yaswanth Reddy <duggempudis919@gmail.com>
2025-12-16 15:45:16 +0530

    Refactor metrobooking.py by removing redundant code

--- metrobooking.py
+++ metrobooking.py
@@ -1,13 +1,13 @@
-#metro ticket booking app
+
 
 import streamlit as st
-import qrcode    #install "pip install qrcode" and "pip install gtts" from cmd
+import qrcode    
 from io import BytesIO
 import uuid
 from PIL import Image
 from gtts import gTTS
 import base64
-#--qr generation function--
+
 def generate_qr(data):
     qr=qrcode.QRCode(version=1,box_size=10,border=4)
     qr.add_data(data)
@@ -15,7 +15,7 @@ def generate_qr(data):
     img=qr.make_image(fill_color="black",back_color="white")
     return img
 
-#--streamlit ui--
+
 
 st.set_page_config(page_title="metro ticket booking")
 st.title("metro ticket booking system with qr+auto voice")
@@ -28,7 +28,7 @@ price_per_ticket=30
 total_amount=no_tickets*price_per_ticket
 st.info(f"total amount:(total_amount)")
 
-#--booking button--
+
 
 if st.button("book ticket"):
     st.error("please enter passenger name")
@@ -37,7 +37,7 @@ elif source==destination:
 else:
     booking_id=str(uuid.uuid4())[:8]
 
-    #--qr code generator--
+    
 
     qr_data=(f"bookingid:(booking_id)\n"f"name:{name}\nfrom:{source}\nTo:{destination}\n tickets:{no_tickets}")
     qr_img=generate_qr(qr_data)
@@ -52,4 +52,5 @@ else:
     st.write(f"**tickets**:{no_tickets}")
     st.write(f"**amount paid**:{total_amount}")
     st.image(qr_bytes,width=250)
+
 
